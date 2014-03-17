d047f4e70ed5761462f544710f13535e69af339e
samui13 <ss.to13@gmail.com>
2014-03-18 00:00:56 +0900

    Bug Not Fixed

--- app/filters.js
+++ app/filters.js
@@ -3,13 +3,17 @@ angular.module('stormFilter',[]).filter('checkBelong',function(){
 	var out = [];
 	for(var key in input){
 	    var postit = input[key];
-	    console.log(status,postit.holding_id,typeof status == 'undefined');
+	    console.log(status == postit.holding_id,status);
+	    if((status == postit.holding_id,status)){
+		continue;
+	    }
+	    /*	    
 	    if(postit.holding_id == "" && typeof status != 'undefined')
 		continue;
-	    /*
-	    if((postit.holding_id != status))
+	    
+	    if(postit.holding_id != "" && (postit.holding_id != status))
 		continue
-	    */
+	    */	    
 	    out.push(postit);
 	}
 	return out;
